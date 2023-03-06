9cf3890eeadd900ce44922b55db9665a9c4e381a
Mulmer <mulmer663@gmail.com>
2023-03-06 20:29:21 +0900

    🔨 주사위 html, js, css 완성
    ajax 비동기 방식으로 애니메이션을 넣어 완성

--- src/main/java/liveeasystreet/dicegame/controller/mulmer663/Son_DiceController.java
+++ src/main/java/liveeasystreet/dicegame/controller/mulmer663/Son_DiceController.java
@@ -1,23 +1,32 @@
 package liveeasystreet.dicegame.controller.mulmer663;
 
 import liveeasystreet.dicegame.domain.Dice;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
 
+@Slf4j
 @Controller
 public class Son_DiceController {
 
     @GetMapping("son_dice")
-    public String son_DiceController(Model model) {
-        Dice dice = new Dice();
-        int randomNum = new Random().nextInt(6);
-
-        dice.setNumber(randomNum);
-        model.addAttribute("dice", dice);
-
+    public String son_DiceController() {
         return "mulmer663/Son_Dice";
     }
+
+    @ResponseBody
+    @PostMapping("son_dice")
+    public Dice makeRandomDiceNum(@ModelAttribute Dice dice) {
+        int randomNum = new Random().nextInt(6) + 1;
+        dice.setNumber(randomNum);
+//        log.info("dice = {}", dice.getNumber());
+
+        return dice;
+    }
+
 }
