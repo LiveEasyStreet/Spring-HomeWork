3ec9e8058d6741c08d65252cfc1254223511e526
TaeKyoung108 <rlaxorud0108@naver.com>
2023-03-09 20:13:22 +0900

    주사위 첫번째 과제 commit

--- src/main/resources/static/js/tk_dice.js
+++ src/main/resources/static/js/tk_dice.js
@@ -33,6 +33,7 @@ const getDiceNum = function() {
     }); //.ajax
 }
 
+let degreeChange = 0;
 const rollingDice = function ({last_number, number, id}) {
     console.log("last_ number : ",last_number);
     console.log("number : ",number);
@@ -41,6 +42,8 @@ const rollingDice = function ({last_number, number, id}) {
     let arr= [[0,0,0],[0,90,0],[180,0,0],[0,270,0],[270,0,0],[90,0,0]]
     // 주사위를 가져옴
     const diceDivision = $(".dice");
+
+    degreeChange = (degreeChange+360)%720;
     console.log(document.querySelector(".dice").style.transform = `rotateX(${arr[number-1][0]}) rotateY(${arr[number-1][1]}deg) rotateZ(${arr[number-1][2]}deg)`);
-    document.querySelector(".dice").style.transform = `rotateX(${arr[number-1][0]}deg) rotateY(${arr[number-1][1]}deg) rotateZ(${arr[number-1][2]}deg)`;
+    document.querySelector(".dice").style.transform = `rotateX(${arr[number-1][0]+degreeChange}deg) rotateY(${arr[number-1][1]+degreeChange}deg) rotateZ(${arr[number-1][2]+degreeChange}deg)`;
 }
