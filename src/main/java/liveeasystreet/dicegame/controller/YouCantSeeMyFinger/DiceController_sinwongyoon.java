28b318c147e8a766c808106c970863ab6845cbd0
gyoon <xmas6904201@gmail.com>
2023-03-10 03:47:38 +0900

    오류해결 : [클래스이름 변경 , return경로 설정 , 주사위값 , math floor삭제] 변경 내용 : 필드변수 제거 후 메소드안에 모두 박아넣음
    #8

--- src/main/java/liveeasystreet/dicegame/controller/YouCantSeeMyFinger/DiceController_sinwongyoon.java
+++ src/main/java/liveeasystreet/dicegame/controller/YouCantSeeMyFinger/DiceController_sinwongyoon.java
@@ -1,34 +1,30 @@
 package liveeasystreet.dicegame.controller.YouCantSeeMyFinger;
 
 import liveeasystreet.dicegame.domain.Dice;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class DiceController {
-
-    public Dice dice;
-
-    public DiceController() {
-        this.dice = new Dice();
-    }
-
+public class DiceController_sinwongyoon {
 
     @GetMapping(value = "/Dice_number")
     public String roll(Model model) {
+
+        Dice dice = new Dice();
+
         int Dice_number;
         // 랜덤숫자 1부터 6까지 임시 저장
-        Dice_number = (int) Math.floor((Math.random() * 6 + 1) * 10);
+        Dice_number = (int) (Math.random() * 6 + 1);
         // 저장완료
-        this.dice.setNumber(Dice_number);
+        dice.setNumber(Dice_number);
         // setter 이용하여 값 넣기
-        model.addAttribute("DiceNumber", this.dice);
+        model.addAttribute("DiceNumber", dice);
         // 타임리프문을 사용하여 객체의 값 출력
         // ex ) ${DiceNumber.number}
+        // domain사용
 
-        return "Dice_number";
+        return "YouCantSeeMyFinger/Dice_number";
     }
 }
