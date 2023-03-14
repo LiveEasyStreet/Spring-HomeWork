22c360b13d6a63e2fc2e80de8ab7648d75deb555
Mulmer <mulmer663@gmail.com>
2023-03-14 19:58:33 +0900

    인터페이스 설계 수정

--- src/main/java/liveeasystreet/dicegame/repository/IDiceRepository.java
+++ src/main/java/liveeasystreet/dicegame/repository/IDiceRepository.java
@@ -9,7 +9,7 @@ public interface IDiceRepository {
     /**
      * 저장 기능 메소드
      * <p>각 숫자가 몇 번 나왔는지 저장한다.</p>
-     * <p>Map구조의 매개변수를 받아 그래도 리포지토리 필드 Map에 반영하면 된다.</p>
+     * <p>diceHistory 매개변수를 받아 깊은 복사로 리포지토리 필드 Map에 반영하면 된다.</p>
      */
     void save(DiceHistory diceHistory);
 
--- src/main/java/liveeasystreet/dicegame/service/IDiceService.java
+++ src/main/java/liveeasystreet/dicegame/service/IDiceService.java
@@ -9,11 +9,11 @@ public interface IDiceService {
 
     /**
      * 각 숫자별 나온 횟수를 기록하는 메소드
-     * <p>매개변수로는 주사위와 주사위기록 두 개를 받는다!</p>
+     * <p>매개변수로는 주사위만 받고 리포지토리의 findAll 메소드 호출 할 것</p>
      * <p>핵심로직 부분이므로 여기서 1 : 0번, 2 : 1번, 3 : 4번 ... 이런 식의 구조를 완성시켜야함</p>
-     * <p>로직 구현 하고 리포지토리의 메소드를 호출할 것</p>
+     * <p>로직 구현 하고 리포지토리의 save 메소드를 호출할 것</p>
      */
-    void save(Dice dice, DiceHistory diceHistory);
+    void save(Dice dice);
 
     /**
      * 숫자별 나온 횟수를 가져오는 메소드
